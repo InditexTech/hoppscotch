2d72ed809acd839980344796e463ec098eea690e
nivedin <nivedinp@gmail.com>
2026-08-05 18:41:47 +0530

    fix: replace operationType string with OperationTypeSchema in GQLTabResponseEventSchema

--- packages/hoppscotch-common/src/services/persistence/validation-schemas/index.ts
+++ packages/hoppscotch-common/src/services/persistence/validation-schemas/index.ts
@@ -618,7 +618,7 @@ const GQLTabResponseEventSchema = z.union([
       type: z.literal("response"),
       time: z.number(),
       operationName: z.optional(z.string()),
-      operationType: z.string(),
+      operationType: OperationTypeSchema,
       data: z.string(),
     })
     .passthrough(),
